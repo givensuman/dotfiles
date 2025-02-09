ac0bcc5e74276f33b375603d0d74b9d637ac06dc
givensuman <givensuman@duck.com>
2025-02-09 18:52:08 -0500

    wip branch made

--- _manager/go.mod
+++ _manager/go.mod
@@ -4,4 +4,7 @@ go 1.23.5
 
 require github.com/pelletier/go-toml/v2 v2.2.3
 
-require github.com/elliotchance/orderedmap/v3 v3.1.0 // indirect
+require (
+	github.com/elliotchance/orderedmap/v3 v3.1.0 // indirect
+	github.com/urfave/cli/v3 v3.0.0-beta1 // indirect
+)
--- _manager/main.go
+++ _manager/main.go
@@ -2,48 +2,41 @@ package main
 
 import (
 	"log"
-	"manager/internal"
+	"manager/cmd"
 	"os"
-	"os/exec"
-	"path/filepath"
-
-	"github.com/pelletier/go-toml/v2"
 )
 
-// (> .___. )>
+// Commands accepted by the CLI
+type Command struct {
+	Name    string
+	Aliases []string
+	Command func()
+}
+
+var cli = []Command{
+	{
+		Name:    "pull local",
+		Aliases: []string{"pl", "pull-local", "pulllocal"},
+		Command: cmd.PullLocal,
+	},
+	{
+		Name:    "pull remote",
+		Aliases: []string{"pr", "pull-remote", "pullremote"},
+		Command: func() { println("TODO") },
+	},
+}
+
+func parseCommand(args []string) (func(), error) {
+	for _, command := range cli {
+		1
+	}
+}
+
 func main() {
-	path, err := filepath.Abs("..")
+	run, err := parseCommand(os.Args[1:])
 	if err != nil {
-		log.Fatal("Unable to get dotfiles directory\n", err)
+		log.Fatal(err)
 	}
 
-	file, err := os.ReadFile(filepath.Join(path, "track.toml"))
-	if err != nil {
-		log.Fatal("Unable to read track.toml\n", err)
-	}
-
-	var config internal.Config
-	err = toml.Unmarshal(file, &config)
-	if err != nil {
-		log.Fatal("Unable to parse TOML configuration\n", err)
-	}
-
-	tracking := internal.ParseTracking(config)
-	if len(tracking) == 0 {
-		log.Fatal("Not tracking anything!\n")
-	}
-
-	for _, value := range tracking {
-		_, err := os.Stat(value)
-		if err != nil {
-			log.Printf("Unable to source tracked file/dir %s, skipping...", value)
-			continue
-		}
-
-		cmd := exec.Command("cp", "-rf", value, path)
-		err = cmd.Run()
-		if err != nil {
-			log.Printf("Unable to copy tracked file/dir %s, skipping...", value)
-		}
-	}
+	println(run)
 }
