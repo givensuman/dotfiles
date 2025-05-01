bbcf17b9ab12e8a57eceb605738e1db300b8da0f
given <givensuman@gmail.com>
2025-05-01 10:04:24 -0400

    update lazyvim

---
+++ nvim/nvim/lua/plugins/hop.lua
@@ -0,0 +1,14 @@
+---@type LazySpec
+return {
+  "phaazon/hop.nvim",
+  config = function()
+    require("hop").setup({
+      keys = "ghfjtyvbrudkcn",
+    })
+  end,
+  keys = {
+    { mode = { "n", "v" }, "<LEADER>hw", "<CMD>HopWord<CR>", desc = "Hop To Word" },
+    { mode = { "n", "v" }, "<LEADER>hl", "<CMD>HopLine<CR>", desc = "Hop To Line" },
+    { mode = { "n", "v" }, "<LEADER>ha", "<CMD>HopAnywhere<CR>", desc = "Hop Anywhere" },
+  },
+}
---
+++ nvim/nvim/lua/plugins/mini-comment.lua
@@ -0,0 +1,14 @@
+---@type LazySpec
+return {
+  {
+    "echasnovski/mini.comment",
+    opts = {
+      mappings = {
+        comment = "<LEADER>/",
+        comment_line = "<LEADER>/",
+        comment_visual = "<LEADER>/",
+        textobject = "<LEADER>/",
+      },
+    },
+  },
+}
